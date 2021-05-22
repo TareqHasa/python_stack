6ce062a0ae32abaea71f7f08b41dfe0fb64887d0
TareqHasa <mr.tareq.ha@gmail.com>
2021-05-22 18:56:01 +0300

    add session

--- dijango/dijango_fundamentals/great_number_game/game_app/views.py
+++ dijango/dijango_fundamentals/great_number_game/game_app/views.py
@@ -1,16 +1,32 @@
-from django.shortcuts import render, redirect,HttpResponse
 import random
+from django.shortcuts import render, redirect
 
-y=random.randint(1, 100)
 
 def index(request):
-    if 'number' in request.session:
-        request.session['number']=request.POST['number_from_user']
-        context={
-            'num' : y
-        }
-        return render(request,"index.html",context)
+    request.session['number']=random.randint(1, 100)
+    print(request.session['number'])
+    return render(request,"index.html")
 
-    else:
-        request.session['number']=0
-        return render(request,'index.html')
+
+def process(request):
+    if request.method == 'POST':
+        number = request.session['number']
+        guess = int(request.POST['number_from_user'])
+
+        if number == guess:
+            request.session['color'] = 'green'
+            print('green')
+            request.session['string'] = f"{number} was the number"
+
+        elif number > guess:
+            request.session['color'] = 'red'
+            request.session['string'] = 'Too low'
+            print('red')
+            
+        else:
+            request.session['color'] = 'red'
+            print('white')
+            request.session['string'] = 'Too high'
+
+    return redirect('/')
+    
