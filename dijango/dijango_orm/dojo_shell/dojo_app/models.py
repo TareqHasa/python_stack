e9acdcd660ff56387afdd080f3662177da50709e
TareqHasa <mr.tareq.ha@gmail.com>
2021-05-24 15:36:39 +0300

    completed the data

--- dijango/dijango_orm/dojo_shell/dojo_app/models.py
+++ dijango/dijango_orm/dojo_shell/dojo_app/models.py
@@ -6,6 +6,7 @@ class Dojo (models.Model):
     name = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     state= models.CharField(max_length=2)
+    desc=models.TextField(default="old dojo")
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now_add=True)
 
